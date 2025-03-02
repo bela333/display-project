02fd2a8002968d27810d9dd899924f5303573146
bela333 <b3kstudio@gmail.com>
2025-03-02 22:45:34 +0100

    fix(apriltag): use resolution independent homography

--- src/app/room/[id]/view/page.tsx
+++ src/app/room/[id]/view/page.tsx
@@ -16,23 +16,41 @@ function ViewingPage({
   room,
   screen,
 }: Readonly<{ room: RoomContextType; screen: ScreenContextType }>) {
+  const [screenBounds, setScreenBounds] = useState([1, 1]);
+  useElementSize(document.body, room.roomID, screen.screenID, (r, s, bounds) =>
+    setScreenBounds([bounds.width, bounds.height])
+  );
+
   const screenLocal = room.lastEvent.screenLocals.find(
     (local) => local.id === String(screen.screenID)
   );
 
   // Row major 2D affine transformation (3x3)
-  const homography = screenLocal?.homography ?? [
+  const inputHomography = screenLocal?.homography ?? [
     [1, 0, 0],
     [0, 1, 0],
     [0, 0, 1],
   ];
 
+  // Column major 2D affine transformation (3x3)
+  let homography = math.transpose(math.matrix(inputHomography));
+
+  // Matrix representing screen size
+  const screenSize = math.matrix([
+    [screenBounds[0] ?? 1, 0, 0],
+    [0, screenBounds[1] ?? 1, 0],
+    [0, 0, 1],
+  ]);
+  const screenSizeInv = math.inv(screenSize);
+
+  homography = math.multiply(screenSizeInv, homography, screenSize);
+
   // Column major 3D affine transformation (4x4)
   let matrix = math.matrix([
-    [homography[0][0], homography[1][0], 0, homography[2][0]],
-    [homography[0][1], homography[1][1], 0, homography[2][1]],
-    [0, 0, homography[2][2], 0],
-    [homography[0][2], homography[1][2], 0, homography[2][2]],
+    [homography.get([0, 0]), homography.get([0, 1]), 0, homography.get([0, 2])],
+    [homography.get([1, 0]), homography.get([1, 1]), 0, homography.get([1, 2])],
+    [0, 0, homography.get([2, 2]), 0],
+    [homography.get([2, 0]), homography.get([2, 1]), 0, homography.get([2, 2])],
   ] as const);
 
   matrix = math.inv(matrix);
@@ -51,6 +69,35 @@ function ViewingPage({
       ></img>
     </Box>
   );
+}
+
+function useElementSize(
+  imageRef: HTMLElement | null,
+  roomID: string,
+  screenID: number,
+  setBounds: (
+    roomID: string,
+    screenID: number,
+    rect: DOMRectReadOnly
+  ) => unknown
+) {
+  useEffect(() => {
+    if (!imageRef || !roomID || !screenID) {
+      return;
+    }
+
+    const observer = new ResizeObserver((entry) => {
+      const rect = entry[0]?.contentRect;
+      if (rect) {
+        setBounds(roomID, screenID, rect);
+      }
+    });
+
+    observer.observe(imageRef);
+    return () => {
+      observer.unobserve(imageRef);
+    };
+  }, [imageRef, roomID, screenID, setBounds]);
 }
 
 function CalibrationPage({
@@ -74,23 +121,7 @@ function CalibrationPage({
 
   const [imageRef, setImageRef] = useState<HTMLImageElement | null>(null);
 
-  useEffect(() => {
-    if (!imageRef || !roomID || !screenID) {
-      return;
-    }
-
-    const observer = new ResizeObserver((entry) => {
-      const rect = entry[0]?.contentRect;
-      if (rect) {
-        setBounds(roomID, screenID, rect);
-      }
-    });
-
-    observer.observe(imageRef);
-    return () => {
-      observer.unobserve(imageRef);
-    };
-  }, [imageRef, roomID, screenID, setBounds]);
+  useElementSize(imageRef, roomID, screenID, setBounds);
 
   const tagID = String(screen.screenID).padStart(5, "0");
   return (
